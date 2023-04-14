362ffee24d6208bf0952b28854f1a4a775f2f299
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-04-13 21:22:15 -0400

    add script for node setup

---
+++ scripts/node-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+/usr/sbin/sshd
+
+# create keys
+CHAINID="athens_7001-1"
+KEYRING="test"
+HOSTNAME=$(hostname)
+echo "HOSTNAME: $HOSTNAME"
+
+# Init a new node to generate genesis file .
+# Copy config files from existing folders which get copied via Docker Copy when building images
+zetacored init Zetanode_HOSTNAME --chain-id=$CHAINID
+rm -rf ~/.zetacored/config/config.toml
+rm -rf ~/.zetacored/config/app.toml
+rm -rf ~/.zetacored/config/client.toml
+cp -r ~/network_files/config/* ~/.zetacored/config/
+
+zetacored keys add operator --algo=secp256k1 --keyring-backend=$KEYRING
+zetacored keys add hotkey --algo=secp256k1 --keyring-backend=$KEYRING
+operator_address=$(zetacored keys show operator -a --keyring-backend=$KEYRING)
+hotkey_address=$(zetacored keys show hotkey -a --keyring-backend=$KEYRING)
+#pubkey=$(zetacored get-pubkey hotkey|sed -e 's/secp256k1:"\(.*\)"/\1/' | sed 's/ //g' )
+echo "operator_address: $operator_address"
+echo "hotkey_address: $hotkey_address"
+#echo "pubkey: $pubkey"
+pubkey=hotkey_address
+mkdir ~/.zetacored/os_info
+jq -n --arg operator_address "$operator_address" --arg hotkey_address "$hotkey_address" --arg pubkey "$pubkey" '{"ObserverAddress":$operator_address,"ZetaClientGranteeAddress":$hotkey_address,"ZetaClientGranteePubKey":$pubkey}' > ~/.zetacored/os_info/os.json
