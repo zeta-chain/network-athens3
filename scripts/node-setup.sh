843ac4657b6ddcda5886836cfbab60c026453ac9
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-04-17 01:19:32 -0400

    reduced steps for genesis

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -14,6 +14,11 @@ rm -rf ~/.zetacored/config/config.toml
 rm -rf ~/.zetacored/config/app.toml
 rm -rf ~/.zetacored/config/client.toml
 cp -a network_files/config/. ~/.zetacored/config/
+cp -a ~/.zetacored/config/genesis.json network_files/config/
+
+pp=$(cat $HOME/.zetacored/config/gentx/z2gentx/*.json | jq '.body.memo' )
+pps=Zetanode_$HOSTNAME
+sed -i -e "/moniker =/s/=.*/= \"$pps\"/" "$HOME"/.zetacored/config/config.toml
 
 zetacored keys add operator --algo=secp256k1 --keyring-backend=$KEYRING
 zetacored keys add hotkey --algo=secp256k1 --keyring-backend=$KEYRING
@@ -27,3 +32,9 @@ mkdir ~/.zetacored/os_info
 jq -n --arg operator_address "$operator_address" --arg hotkey_address "$hotkey_address" --arg pubkey "$pubkey" '{"ObserverAddress":$operator_address,"ZetaClientGranteeAddress":$hotkey_address,"ZetaClientGranteePubKey":$pubkey}' > ~/.zetacored/os_info/os.json
 mkdir -p genesis_files/os_info
 cp ~/.zetacored/os_info/os.json ./genesis_files/os_info/os_$HOSTNAME.json
+
+zetacored collect-observer-info
+zetacored add-observer-list
+zetacored gentx operator 1000000000000000000000azeta --chain-id=$CHAINID --keyring-backend=$KEYRING
+mkdir -p genesis_files/gentx
+cp ~/.zetacored/config/gentx/* ./genesis_files/gentx/
--- scripts/start-zetaclient.sh
+++ scripts/start-zetaclient.sh
@@ -4,6 +4,31 @@ KEYRING="test"
 HOSTNAME=$(hostname)
 
 
+clibuilder()
+{
+   echo ""
+   echo "Usage: $0 -k KeygenBlock"
+   echo -e "\t-k Keygen Block for zetaclient"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "k:" opt
+do
+   case "$opt" in
+      k ) KeygenBlock="$OPTARG" ;;
+      ? ) clibuilder ;; # Print cliBuilder in case parameter is non-existent
+   esac
+done
+
+if [ -z "$KeygenBlock" ]
+then
+   echo "Some or all of the parameters are empty";
+   clibuilder
+fi
+
+
+
+echo "KeygenBlock: $KeygenBlock"
 operatorAddress=$(zetacored keys show operator -a --keyring-backend=test)
 echo "operatorAddress: $operatorAddress"
 echo "Start zetaclientd"
@@ -11,5 +36,5 @@ rm ~/.tss/*
 export TSSPATH=~/.tss
 zetaclientd init --enable-chains "goerli_localnet" \
   --pre-params ~/preParams.json \
-  --chain-id $CHAINID --dev --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block 100
+  --chain-id $CHAINID --dev --operator "$operatorAddress" --log-level 0 --hotkey=hotkey --keygen-block $KeygenBlock
 zetaclientd start ~/.zetacored/zetaclient.log 2>&1  &
