12030fbbb963125c2c9fd3c4565be302b4c29c12
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-10 14:05:07 -0400

    Modify params (#28)

--- scripts/genesis_collector.sh
+++ scripts/genesis_collector.sh
@@ -2,6 +2,30 @@
 CHAINID="athens_7001-1"
 KEYRING="test"
 
+
+clibuilder()
+{
+   echo ""
+   echo "Usage: $0 -k KeygenBlock"
+   echo -e "\t-k KeygenBlock"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "k:" opt
+do
+   case "$opt" in
+      k ) KeygenBlock="$OPTARG" ;;
+      ? ) clibuilder ;; # Print cliBuilder in case parameter is non-existent
+   esac
+done
+
+if [ -z "$KeygenBlock" ]
+then
+   echo "Some or all of the parameters are empty";
+   clibuilder
+fi
+
+
 rm -rf ~/.zetacored/os_info/*
 rm -rf ~/.zetacored/config/gentx/*
 
@@ -16,11 +40,11 @@ cat "$HOME"/.zetacored/config/genesis.json | jq '.app_state["gov"]["deposit_para
 cat "$HOME"/.zetacored/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="azeta"' > "$HOME"/.zetacored/config/tmp_genesis.json && mv "$HOME"/.zetacored/config/tmp_genesis.json "$HOME"/.zetacored/config/genesis.json
 cat "$HOME"/.zetacored/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="azeta"' > "$HOME"/.zetacored/config/tmp_genesis.json && mv "$HOME"/.zetacored/config/tmp_genesis.json "$HOME"/.zetacored/config/genesis.json
 cat "$HOME"/.zetacored/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > "$HOME"/.zetacored/config/tmp_genesis.json && mv "$HOME"/.zetacored/config/tmp_genesis.json "$HOME"/.zetacored/config/genesis.json
-cat "$HOME"/.zetacored/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="10s"' > "$HOME"/.zetacored/config/tmp_genesis.json && mv "$HOME"/.zetacored/config/tmp_genesis.json "$HOME"/.zetacored/config/genesis.json
+cat "$HOME"/.zetacored/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="200s"' > "$HOME"/.zetacored/config/tmp_genesis.json && mv "$HOME"/.zetacored/config/tmp_genesis.json "$HOME"/.zetacored/config/genesis.json
 
 
 zetacored collect-observer-info
-zetacored add-observer-list
+zetacored add-observer-list --keygen-block="$KeygenBlock"
 zetacored collect-gentxs
 zetacored validate-genesis
 rm -rf network_files/config/genesis.json
--- scripts/start-zetacore.sh
+++ scripts/start-zetacore.sh
@@ -3,6 +3,27 @@ CHAINID="athens_7001-1"
 KEYRING="test"
 HOSTNAME=$(hostname)
 
+if grep -q 'moniker = ""' network_files/config/config.toml; then
+    echo You must set a moniker for this validator
+    # Get user input
+    read -p "Enter the moniker for this validator: " input
+
+    # Print input and confirm
+    echo "You entered: $input"
+    read -p "Is this correct? [Y/n] " confirm
+
+    # Check confirmation
+    if [[ $confirm =~ ^[Yy]$ ]]; then
+        echo "Confirmed!"
+    else
+        echo "Aborted."
+        exit 1
+    fi
+
+    # Replace moniker
+    sed -i "s/moniker = \"test\"/moniker = \"$input\"/g" network_files/config/config.toml
+fi
+
 rm -rf ~/.zetacored/config/genesis.json
 cp -a network_files/config/genesis.json ~/.zetacored/config/
 zetacored start --pruning=nothing --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable >> ~/.zetacored/zetacored.log 2>&1  &
