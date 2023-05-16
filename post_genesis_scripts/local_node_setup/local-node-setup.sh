66313f49c4bfbd5df6634b904adc9db725e9695a
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-05-16 15:46:35 -0400

    remove peers details from script

--- post_genesis_scripts/local_node_setup/local-node-setup.sh
+++ post_genesis_scripts/local_node_setup/local-node-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 CHAINID="athens_7001-1"
 KEYRING="test"
-
+peerip=""
+peername=""
 rm -rf ~/.zetacored
 zetacored init Zetanode-LocalNode --chain-id=$CHAINID
 rm -rf ~/.zetacored/config/genesis.json
 
-curl 34.239.99.239:26657/genesis | jq .result.genesis > ~/.zetacored/config/genesis.json
-pp=af58c82b5f4d2268e0b8ca9150190e438c07d90d@34.239.99.239:26656,038234610497601373b1d27e27251674c6c81df7@3.218.170.198:26656
+curl $peerip:26657/genesis | jq .result.genesis > ~/.zetacored/config/genesis.json
+pp=$peername@$peerip:26656
 sed -i -e "/persistent_peers =/s/=.*/= \"$pp\"/" "$HOME"/.zetacored/config/config.toml
 zetacored start
 
-#sed -i -e "/seeds =/s/=.*/= \"$pp\"/" "$HOME"/.zetacored/config/config.toml
