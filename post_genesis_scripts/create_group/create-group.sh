7e655df6dd4e76d0a008d938d2b5030e49522f2f
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2023-05-24 10:30:56 -0600

    fixed merge conflicts

--- post_genesis_scripts/create_group/create-group.sh
+++ post_genesis_scripts/create_group/create-group.sh
@@ -9,13 +9,8 @@ node=tcp:$nodeip//:26657
 signerAddress=$(zetacored keys show $signer -a --keyring-backend=test)
 echo "signerAddress: $signerAddress"
 
-<<<<<<< HEAD
-zetacored tx  group create-group-with-policy "$signerAddress" group-metadata group-policy-metadata post_genesis_scripts/create_group/members.json post_genesis_scripts/create_group/policy.json --from $signer --fees=40azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block
-zetacored q group group-policies-by-group 1
-=======
 zetacored tx  group create-group-with-policy "$signerAddress" group-metadata group-policy-metadata post_genesis_scripts/create_group/members.json post_genesis_scripts/create_group/policy.json --from $signer --fees=40azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --node=$node
 zetacored q group group-policies-by-group 1 --node=$node
->>>>>>> main
 
 
 
