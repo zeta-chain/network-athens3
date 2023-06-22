a2ff115bb69b3e0fb39c1ff7eb940873919db0a2
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-06-22 15:56:51 -0400

    add a script to set voting period

---
+++ post_genesis_scripts/create_proposal/create-exec-gov.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CHAINID="athens_7001-1"
+KEYRING="test"
+HOSTNAME=$(hostname)
+signer="val0_op_se"
+nodeip="3.218.170.198"
+node=tcp://$nodeip:26657
+
+
+signerAddress=$(zetacored keys show $signer -a --keyring-backend=test)
+echo "signerAddress: $signerAddress"
+
+
+zetacored tx gov submit-legacy-proposal param-change proposal_voting_period.json --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block
+#zetacored tx gov deposit 3 10000000azeta --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block
+#zetacored tx bank send $signerAddress zeta1utsn7w4dzluh026ylwkntmtc4jrpn2393nsggp 10zeta --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block
+
