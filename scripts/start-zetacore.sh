84656add5f6cfdc0851411d7fb7c821ab01fd914
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-08-30 14:27:21 -0400

    generate new genesis.json

--- scripts/start-zetacore.sh
+++ scripts/start-zetacore.sh
@@ -3,8 +3,12 @@ CHAINID="athens_7001-1"
 KEYRING="test"
 HOSTNAME=$(hostname)
 
+rm -rf ~/.zetacored/data/*
+cp -a network_files/data/*.json ~/.zetacored/data/
+
+rm -rf ~/.zetacored/config/addrbook.json
 
 rm -rf ~/.zetacored/config/genesis.json
 cp -a network_files/config/genesis.json ~/.zetacored/config/
-cp -a network_files/data/*.json ~/.zetacored/data/
+
 zetacored start --pruning=nothing --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable >> ~/.zetacored/zetacored.log 2>&1  &
