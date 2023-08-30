d1e5c770c59e79c6bc231d85d177a925a80402ed
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-08-30 12:17:33 -0400

    change params

--- post_genesis_scripts/create_proposal/create-exec-gov.sh
+++ post_genesis_scripts/create_proposal/create-exec-gov.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-CHAINID="athens_7001-1"
+CHAINID="athens_7001-2"
 KEYRING="test"
 HOSTNAME=$(hostname)
-signer="val0_op_se"
+signer="operator"
 nodeip="3.218.170.198"
 node=tcp://$nodeip:26657
 
@@ -11,7 +11,7 @@ signerAddress=$(zetacored keys show $signer -a --keyring-backend=test)
 echo "signerAddress: $signerAddress"
 
 
-zetacored tx gov submit-legacy-proposal param-change proposal_test_params.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block --gas=auto --gas-adjustment=1.5 --gas-prices=0.001azeta
+zetacored tx gov submit-legacy-proposal param-change gov_observer_params_supported_chains.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --gas=auto --gas-adjustment=1.5 --gas-prices=0.001azeta
 #zetacored tx gov deposit 3 10000000azeta --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block
 #zetacored tx bank send $signerAddress zeta1utsn7w4dzluh026ylwkntmtc4jrpn2393nsggp 10zeta --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --node=$node --broadcast-mode=block
 
