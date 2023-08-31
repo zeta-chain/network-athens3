ef0cf4a6751b9aa2712ef8617a493717df7df154
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-08-31 17:03:03 -0400

    config lib

--- post_genesis_scripts/create_proposal/create-exec.sh
+++ post_genesis_scripts/create_proposal/create-exec.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-CHAINID="athens_7001-2"
+CHAINID="athens_7001-1"
 KEYRING="test"
 HOSTNAME=$(hostname)
-signer="operator"
+signer="tanmay"
 nodeip="3.218.170.198"
 #node="tcp://3.218.170.198:26657"
 node="tcp://46.4.15.110:26657"
@@ -32,11 +32,11 @@ signerAddress=$(zetacored keys show $signer -a --keyring-backend=test)
 echo "signerAddress: $signerAddress"
 
 #zetacored q group proposals-by-group-policy zeta1afk9zr2hn2jsac63h4hm60vl9z3e5u69gndzf7c99cqge3vzwjzsxn0x73 --node=$node
-zetacored tx group submit-proposal post_genesis_scripts/create_proposal/proposal_stop_inbound_cctx.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=sync --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta
+zetacored tx group submit-proposal post_genesis_scripts/create_proposal/proposal_add_observer.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --gas=auto --gas-adjustment=1.5 --node=$node --gas-prices=0.01azeta
 
 sleep 10
-zetacored tx group vote "$PID" $signerAddress VOTE_OPTION_YES metadata --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block
+zetacored tx group vote 128 $signerAddress VOTE_OPTION_YES metadata --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --node=$node
 
-zetacored tx group exec "$PID" --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block
+zetacored tx group exec 128 --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --node=$node
 zetacored q block | jq .block.header.height
 
