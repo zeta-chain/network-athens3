7e10db7195862d393b578e9d8c0b68ee1bf6326a
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-10-11 14:40:09 -0400

    add genesis files

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -7,3 +7,9 @@ rm -rf /usr/local/bin/zetaclientd
 cp ~/go/bin/zetaclientd  /usr/local/bin/
 rm -rf /usr/local/bin/zetacored
 cp ~/go/bin/zetacored  /usr/local/bin/
+
+curl -X 'GET' \
+  'https://localhost:1317/zeta-chain/crosschain/pendingNonces' \
+  -H 'accept: application/json'
+
+https://18.211.188.24:1317/zeta-chain/crosschain/pendingNonces
---
+++ scripts/debug-zetacore.sh
@@ -0,0 +1,7 @@
+killall zetacored
+git pull
+make install-testnet
+rm -rf /usr/local/bin/zetacored
+cp ~/go/bin/bin/zetacored  /usr/local/bin/
+zetacored start --pruning=nothing --minimum-gas-prices=0.0001azeta --json-rpc.api eth,txpool,personal,net,debug,web3,miner --api.enable >> ~/.zetacored/zetacored.log 2>&1  &
+
