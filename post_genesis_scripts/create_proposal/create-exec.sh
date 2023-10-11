189586bc1531c389025f9e53c707fb6c259ca901
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-10-11 10:59:14 -0400

    add genesis files

--- post_genesis_scripts/create_proposal/create-exec.sh
+++ post_genesis_scripts/create_proposal/create-exec.sh
@@ -31,7 +31,7 @@ signerAddress=$(zetacored keys show $signer -a --keyring-backend=test)
 echo "signerAddress: $signerAddress"
 
 #zetacored q group proposals-by-group-policy zeta1afk9zr2hn2jsac63h4hm60vl9z3e5u69gndzf7c99cqge3vzwjzsxn0x73 --node=$node
-zetacored tx group submit-proposal post_genesis_scripts/create_proposal/proposal_migrate_tss_funds.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --gas=auto --gas-adjustment=1.5 --node=$node --gas-prices=0.01azeta
+zetacored tx group submit-proposal post_genesis_scripts/create_proposal/proposal_keygen.json --from $signer --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --gas=auto --gas-adjustment=1.5 --node=$node --gas-prices=0.01azeta
 
 zetacored tx group vote $PID $signerAddress VOTE_OPTION_YES metadata --from $signer --gas=auto --gas-adjustment=1.5 --gas-prices=0.01azeta --chain-id=$CHAINID --keyring-backend=$KEYRING -y --broadcast-mode=block --node=$node
 
--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -2,3 +2,8 @@ rm -rf /usr/local/bin/zetacored
 rm -rf /usr/local/bin/zetaclientd
 cp ~/go/bin/bin/zetacored  /usr/local/bin/
 cp ~/go/bin/bin/zetaclientd  /usr/local/bin/
+
+rm -rf /usr/local/bin/zetaclientd
+cp ~/go/bin/zetaclientd  /usr/local/bin/
+rm -rf /usr/local/bin/zetacored
+cp ~/go/bin/zetacored  /usr/local/bin/
