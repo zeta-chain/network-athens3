9b52258b45654433133bd68afafffe6ef77f5f0f
Tanmay <tanmay.bhattacharya.smit@gmail.com>
2023-10-10 21:53:32 -0400

    add genesis files

--- scripts/copy-binaries.sh
+++ scripts/copy-binaries.sh
@@ -1,4 +1,4 @@
 rm -rf /usr/local/bin/zetacored
 rm -rf /usr/local/bin/zetaclientd
-cp ~/go/bin/zetacored  /usr/local/bin/
-cp ~/go/bin/zetaclientd  /usr/local/bin/
+cp ~/go/bin/bin/zetacored  /usr/local/bin/
+cp ~/go/bin/bin/zetaclientd  /usr/local/bin/
